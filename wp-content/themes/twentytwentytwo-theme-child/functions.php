fa1d894ab13a2c18f0f79941fcddf0e713f20700
amirestes <amirestes@gmail.com>
2022-07-26 21:21:41 -0500

    added bootstrap

--- wp-content/themes/twentytwentytwo-theme-child/functions.php
+++ wp-content/themes/twentytwentytwo-theme-child/functions.php
@@ -16,3 +16,17 @@ function twentytwentytwo_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'twentytwentytwo-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'twentytwentytwo_child_scripts' );
+
+function bootstrap_enqueue_styles() {
+    wp_register_style('bootstrap',get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    $dependencies = array('bootstrap');
+    wp_enqueue_style( 'bootstrap-style', get_stylesheet_directory_uri(), $dependencies); 
+}
+
+function bootstrap_enqueue_scripts() {
+    $dependencies = array('jquery');
+    wp_enqueue_script('bootstrap-script', get_stylesheet_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'bootstrap_enqueue_styles' ,1);
+add_action( 'wp_enqueue_scripts', 'bootstrap_enqueue_scripts',1 ); 
