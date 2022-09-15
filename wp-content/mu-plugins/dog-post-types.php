4498f4b0b3c7790028d8840875b5ab55a00e7404
amirestes <amirestes@gmail.com>
2022-09-14 23:00:52 -0500

    added custom post type must use plugins

--- wp-content/mu-plugins/dog-post-types.php
+++ wp-content/mu-plugins/dog-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function create_custom_post_types() {
+    //Dog Post Type
 	register_post_type('dogs',
 		array(
 			'labels'=>array(
@@ -11,8 +12,26 @@ function create_custom_post_types() {
 				'all_items' => 'All Dogs'
 			),
 			'public'=> true,
+			// 'has_archive'=> true,
+			// 'rewrite'=>array('slug'=>'dogs'),
+            'menu-icon' => 'dashicons-pets'
+		),
+		
+	);
+
+    //Litter Post Type
+    register_post_type('litters',
+		array(
+			'labels'=>array(
+				'name'=>__('Litters'),
+				'singular_name'=>__('Litter'),
+				'add_new_item' => 'Add New Litter',
+				'edit_item' => 'Edit Litter',
+				'all_items' => 'All Litters'
+			),
+			'public'=> true,
 			'has_archive'=> true,
-			'rewrite'=>array('slug'=>'dogs')
+			'rewrite'=>array('slug'=>'litters')
 		),
 		
 	);
